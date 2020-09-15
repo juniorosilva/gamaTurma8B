1d3da2353ee08ecbd2535166b86cedfc4884dab8
juniorosilva <edson.adsjr@gmail.com>
2020-09-15 11:04:23 -0300

    aula 06

---
+++ aula06/appRelogio.java
@@ -0,0 +1,16 @@
+package aula06;
+
+/**
+ * appRelogio
+ */
+public class appRelogio {
+    public static void main(String[] args) {
+    
+    Relogio h = new Relogio();
+    
+    for(int i = 0 ; i < 120 ; i++){
+        h.setHorario(i);
+        System.out.println(h.getHora());
+        }
+    }
+}
